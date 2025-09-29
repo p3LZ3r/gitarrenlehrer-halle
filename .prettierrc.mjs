120de72e6991e1ca7d82fa8466df595921000f1e
Torsten Linnecke <torsten@enode.ai>
2025-09-29 11:48:51 +0200

    chore(deps): switch from pnpm to yarn and simplify configs
    - Remove pnpm-lock.yaml and update yarn.lock
    - Delete .prettierrc.js configuration file
    - Add formatting to package.json lint-staged config
    - Simplify .husky/pre-commit hook
    - Format eslint.config.js for consistency

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+import * as tailwindPlugin from 'prettier-plugin-tailwindcss';
+
+export default {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  plugins: [tailwindPlugin],
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const compat = new FlatCompat();
-
-module.exports = [
-  ...compat.extends('next/core-web-vitals'),
-  {
-    rules: {
-      'react-hooks/rules-of-hooks': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      '@next/next/no-duplicate-head': 'off',
-      '@next/next/no-page-custom-font': 'off',
-      'react/display-name': 'off',
-    },
-  },
-];
+ const { FlatCompat } = require('@eslint/eslintrc');
+ const compat = new FlatCompat();
+ 
+ module.exports = [
+   ...compat.extends('next/core-web-vitals'),
+   {
+     rules: {
+       'react-hooks/rules-of-hooks': 'off',
+       'react-hooks/exhaustive-deps': 'off',
+       '@next/next/no-duplicate-head': 'off',
+       '@next/next/no-page-custom-font': 'off',
+       'react/display-name': 'off',
+     },
+   },
+ ];
